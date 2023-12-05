d4a5d1e07b65ce1de1a4602b5b70b040af6053ee
shubhat33 <rashidshubhat41@gmail.com>
2023-12-05 18:34:37 +0530

    added recursion definition

--- factorial.py
+++ factorial.py
@@ -13,3 +13,8 @@ def factorialByIteration(number):
 
 print(factorialByIteration(5))
 print(factorialByRecursion(5))
+
+# Recursion is a method of looping using a function untill the base case
+# is trigerred which terminated the loop by returning something...
+# A recursive function solves a particular problem by calling a copy
+# of itself and solving smaller subproblems of the original problems.
