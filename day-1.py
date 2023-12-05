690bc0ad4ccdc2a15ed467e6447752e97134cf69
shubhat33 <rashidshubhat41@gmail.com>
2023-12-05 21:43:52 +0530

    Introduction to Algorithms

---
+++ day-1.py
@@ -0,0 +1,31 @@
+'''
+
+ALGORITHMS:
+
+Set of instructions or steps to solve a problem.
+  OverTime some algorithms have been observed to perform
+  better than others and thus are widely used in cs and have
+  been given specific names like binary search.
+
+The most important part of writing a particular algorithm is to
+  know when to use it, which can be found out if we know what
+  was the problem which led to the wide-scale acceptance of that
+  particular algorithm and hence a definitive problem statement
+  is required.
+
+A Good Algorithm is :
+- Correct :
+    - This means the algorithm must accept an input ,perform certain strategic
+      operations on it and come up with an output in a reasonable amount
+      of time.
+
+- Efficient :
+    - This is measured in two ways :
+        - Space :
+            - How much space does the algorithm require completing and coming
+              with a result.This can depend on hardware but a good algorithm
+              must perform under all circumstances and hence a good algorithm
+              takes as less space as possible.
+        - Time :
+
+'''
