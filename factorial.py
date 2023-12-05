d53be24dd288b24cfc586685c417ad2e645956f7
shubhat33 <rashidshubhat41@gmail.com>
2023-12-05 19:20:40 +0530

    added permutations algorithm using recursion

--- factorial.py
+++ factorial.py
@@ -1,3 +1,12 @@
+# RECURSION :
+# Recursion is a method of looping using a function until the base case
+# is triggered which terminated the loop by returning something...
+# A recursive function solves a particular problem by calling a simpler copy
+# of itself and solving smaller sub-problems of the original problems.
+
+# examples and comparison :
+
+# factorial :
 def factorialByRecursion(number):
     if number == 1:
         return 1
@@ -11,10 +20,17 @@ def factorialByIteration(number):
         res *= i
     return res
 
-print(factorialByIteration(5))
-print(factorialByRecursion(5))
+# permutations
+def permutations(string,pocket=""): #recursion
+    if len(string) == 0:
+        print(pocket)
+    else:
+        for i in range(len(string)):
+            letter = string[i]
+            back = string[i+1:]
+            front = string[:i]
+            together = front + back
+            print(letter,front,back,together,pocket)
+            permutations(together,pocket+letter)
 
-# Recursion is a method of looping using a function untill the base case
-# is trigerred which terminated the loop by returning something...
-# A recursive function solves a particular problem by calling a copy
-# of itself and solving smaller subproblems of the original problems.
+
