f464c426a031c49f4f92d8ed0cd2948845581429
shubhat33 <rashidshubhat41@gmail.com>
2023-12-10 21:46:28 +0530

    Add Binary Tree

---
+++ BinaryTree.py
@@ -0,0 +1,58 @@
+"""
+Binary Tree :
+    A data struction consisting of nodes where each node has atmost
+    two children generally represented as left and right.Node with
+    no parent is called Root and node with no children is called leaf.
+    A binary tree has :
+        - almost 2 children
+        - a unique path towards each node
+"""
+
+
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+# CREATE NODES
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+# LINK THE NODES
+a.left = b
+a.right = c
+
+b.left = d
+b.right = e
+
+c.left = f
+
+"""
+Depth First Traversal :
+    Traversing the tree by going in depth first and then laterally.
+    Can be done either using iteration of recursion
+"""
+
+# DFT using iteration
+def dft(root):
+    stack = [root]
+    result = []
+    while stack:
+        temp = stack[0]
+        result.append(temp.val)
+        if temp.left:
+            stack.append(temp.left)
+        if temp.right:
+            stack.append(temp.right)
+        temp = stack.pop(0)
+    return result
+
+
+
+
