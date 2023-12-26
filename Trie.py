2456f0a482216aea0dccea623e6d4ffc64f3b8db
shubhat33 <rashidshubhat41@gmail.com>
2023-12-26 16:26:45 +0530

    define trie

---
+++ Trie.py
@@ -0,0 +1,15 @@
+"""
+TRIE DATA STRUCTURE :
+- Also known as prefix tree.
+- A tree like data structure used to store strings. The idea
+  is to put string with same prefix in similar branch.Trie makes
+  searching strings with common prefixes easier like the autocomplete suggestions that
+  pop while using google search box or the spell checker in keyboards.
+
+  image : "https://media.geeksforgeeks.org/wp-content/uploads/20220828232752/Triedatastructure1.png"
+
+- Unlike binary tree a trie node can have any number of children.
+"""
+
+
+
