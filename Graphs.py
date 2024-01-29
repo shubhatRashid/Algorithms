129db536b50ae547f8fc21f60a88bb578e2e6094
shubhat33 <rashidshubhat41@gmail.com>
2024-01-29 20:55:25 +0530

    start graphs

---
+++ Graphs.py
@@ -0,0 +1,21 @@
+"""
+GRAPHS:
+
+- A graph is a nonlinear data structure consisting of nodes(vertices)
+    which are connected using edges(arcs).The nodes can be connected
+    in any order.
+- A city can be considered as graph where all the lanes are
+    edges/arcs which connect the junctions of the city which
+    represent as nodes/vertices.
+
+GRAPH TERMINOLOGIES:
+
+- Vertices : Nodes of graph.
+- Edges : connecting lines between nodes.
+- UnWeighted Graph : Edges do not have any weights associated with them.
+- Undirected Graph : Edges do not have directions associated with them.
+- Cyclic Graph : contains at least one loop.
+- Tree : Special type of graph which is acyclic,Unweighted and directed.
+
+
+"""
