cf6b92c45219d46e3d7bfdd91c624cb5a01b3790
shubhat33 <rashidshubhat41@gmail.com>
2024-03-25 15:45:52 +0530

    grid traveller using tabulation

--- dynamicProgramming/Tabulation.py
+++ dynamicProgramming/Tabulation.py
@@ -10,4 +10,20 @@ def fibTab(n):
         if i+2 < len(table):
             table[i+2] += table[i]
     return table[-1]
-print(fibTab(7))
+
+# gridTraveller using tabulation
+def gridTravel(m,n):
+    table = [[0 for i in range(m+1)] for j in range(n+1)]
+    table[1][1] = 1
+    for i in range(m+1):
+        for j in range(n+1):
+            current = table[i][j]
+            if i+1 <= m:
+                table[i+1][j] += current
+            if j+1 <= n:
+                table[i][j+1] += current
+
+    print (table)
+    return table[m][n]
+
+print(gridTravel(3,3))
